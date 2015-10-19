00a4ec597ee567325844bd60373db0e394d86b6b
camilo <capulido@minambiente.gov.co>
2015-10-19 16:23:22 -0500

    update functionality todo: check value email format

--- js/form_validator.js
+++ js/form_validator.js
@@ -1,14 +1,76 @@
 (function() {
 
-    this.FormValidator = function() {
+    // counter fields
+
+    var counterFields = 0;
+    var counterErrros = 0;
+    var spanError  = "<span class='error-label-form-validator'>";
+    var spanErrorFin  = "</span>";
+    var ERROR_ES = "Error el campo es requerido ";
+    // Define function constructor
+    this.AjaxValidator = function(params) {
+
+       if(params === null || params === undefined || checkObject(params) === 0){
+                console.log(" Error atribute params can not be empty ");
+                return false;
+         }else{
+                // clear existing errors
+                $('.error-label-form-validator').remove();
+
+                // check if has fields param
+                if(params.hasOwnProperty('fields') === true ){
+
+                    counterFields =  params.fields.length;
+
+                    for(var counter = 0; counter < params.fields.length;counter++ ){
+
+                        counterErrros += validateSize(params.fields[counter]);
+
+                    }
+
+
+
+                }else{
+
+                    console.log(" The validation form need the fields specification ");
+                    return false;
+                }
+
+
+       }
+    }
+
+
+    // library functions
+    AjaxValidator.prototype.getValidation = function() {
+
+        return counterErrros > 0 ? false : true ;
 
     }
 
 
+    // Internal lib funciones
 
-    FormValidator.prototype.init = function(){
+    /*
+        Validate size content field
+    */
 
+    function validateSize(field){
+        if( $('#'+field).val() === "" || $('#'+field).val()  === null   ){
+
+                $(spanError+ERROR_ES+spanErrorFin).insertAfter("#"+field).css('color','red');
+
+                return 1;
+        }
+
+        return 0;
     }
 
+    /*
+        Check if object is empty
+    */
+    function checkObject(obj){
+        return Object.keys(obj).length;
+    }
 
 }());
